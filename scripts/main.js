2d5afd37055fea75db8653317b8609aea99669ca
sinchang <sinchangwen@gmail.com>
2019-05-30 17:03:46 +0800

    Support storage sync

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,11 @@
 'use strict'
 
-const AK = ''
-const SK = ''
-
-function fetchRandomWord() {
-  return fetch('/scripts/words.json')
-    .then(function (response) {
-      return response.json()
-    })
-    .then(function (data) {
-      data = data.data
-      const knewWords = localStorage.getItem('knewWords') || ''
-      let randomWord = data[Math.floor(Math.random() * data.length)]
-
-      while (knewWords.indexOf(randomWord) > -1) {
-        randomWord = data[Math.floor(Math.random() * data.length)]
-      }
-
-      return randomWord
-    })
-}
+const explainEle = document.getElementById('explain')
+const linkEle = document.getElementById('webLink')
+const wordEle = document.getElementById('word')
 
 function translateWord(word) {
-  const salt = 2
-  const md5Str = md5(`${AK}${word}${salt}${SK}`)
-  const url = `https://cors-anywhere.herokuapp.com/http://openapi.youdao.com/api?q=${word}&from=EN&to=zh_CHS&appKey=${AK}&salt=${salt}&sign=${md5Str}`
+  const url = `http://api.sinchang.me/youdao/translate?word=${word}`
 
   return fetch(url)
     .then(function (response) {
@@ -36,35 +17,41 @@ function translateWord(word) {
 }
 
 function skip(word) {
-  let knewWords = localStorage.getItem('knewWords') || ''
-
-  if (knewWords) knewWords = knewWords.split(',')
-  if (!knewWords.includes(word)) {
-    localStorage.setItem('knewWords', [...knewWords, word])
-  }
-
-  main()
+  chrome.storage.sync.get(['englishTabWords'], function(result) {
+    const words = result.englishTabWords
+    words.forEach((item, index) => {
+      if (item === word) words.splice(index, 1)
+    })
+    
+    chrome.storage.sync.set({ englishTabWords: words }, function() {
+      main()
+    })
+  })
 }
 
 function main() {
-  fetchRandomWord().then(word => {
-    translateWord(word).then(res => {
-      document.getElementById('word').innerText = word
+  chrome.storage.sync.get(['englishTabWords'], function(result) {
+    if (!result.englishTabWords.length) return
+    const words = result.englishTabWords
+    const word = words[Math.floor(Math.random() * words.length)]
 
+    translateWord(word).then(res => {
+      wordEle.innerText = word
+  
       const explains = res.basic.explains
       const htmlStr = explains.map(item => {
         return `<li>${item}</li>`
       })
-
-      const linkEle = document.getElementById('webLink')
+  
       linkEle.style.display = 'inline-block'
       linkEle.setAttribute('href', `http://www.youdao.com/w/eng/${word}`)
-
-      document.getElementById('explain').innerHTML = htmlStr.join('')
-      document.getElementById('skip').addEventListener('click', function () {
-        skip(word)
-      }, false)
-    })
+  
+      explainEle.innerHTML = htmlStr.join('')
+  })
+  
+    document.getElementById('skip').addEventListener('click', function () {
+      skip(word)
+    }, false)
   })
 }
 
---
+++ scripts/options.js
@@ -0,0 +1,30 @@
+const list = document.getElementById('list')
+const addBtn = document.getElementById('addBtn')
+const wordTextarea = document.getElementById('wordTextarea')
+let globalWords = ''
+
+main()
+
+addBtn.addEventListener('click', function() {
+  globalWords = wordTextarea.value.split('\n')
+  chrome.storage.sync.set({ englishTabWords: globalWords }, function() {
+    renderList()
+    wordTextarea.value = ''
+  });
+})
+
+function main() {
+  chrome.storage.sync.get(['englishTabWords'], function(result) {
+    globalWords = result.englishTabWords
+    renderList()
+  })
+}
+
+function renderList() {
+  let listHtml = ''
+  globalWords.forEach(word => {
+    listHtml += `<li class="list-group-item">${word.trim()}</li>`
+  })
+
+  list.innerHTML = listHtml
+}
